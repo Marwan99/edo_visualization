7eaea52310d4da3b8d677e2c6f5a9999dbd0d0ad
Marwan99 <marokhaled99@gmail.com>
2019-09-02 20:15:47 +0100

    Initial commit

---
+++ src/server.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+#rostopic pub /listener std_msgs/UInt32 3
+import rospy
+from edo_core_msgs.msg import CartesianPose
+#from std_msgs.msg import UInt32
+from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, url_for, copy_current_request_context
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+#app.config['DEBUG'] = True
+
+socketio = SocketIO(app)
+
+def ros_callback(msg):
+    socketio.emit('newnumber', {'number': msg.x}, namespace='/test')
+    #print(msg)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    print("---------------*Connected")
+    rospy.init_node('lololol', anonymous=True, disable_signals=True)
+    rospy.Subscriber('/cartesian_pose', CartesianPose, ros_callback)
+    rospy.loginfo("WebSocket started")
+    '''
+    while not rospy.is_shutdown():
+        rospy.loginfo("2")
+        rospy.spin()
+        '''
+    #return "1"
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected')
+
+rospy.loginfo(str(__name__))
+
+if __name__ == '__main__':
+    socketio.run(app)
